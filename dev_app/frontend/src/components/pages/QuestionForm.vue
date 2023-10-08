ab6171f0c3f9b922072f6871c2ca966dd20dd9a4
silbull <tmiya5382@gmail.com>
2023-10-08 13:25:42 +0900

    cropperjsをinstall．ファイル選択後，画像のトリミングをする機能を追加

--- dev_app/frontend/src/components/pages/QuestionForm.vue
+++ dev_app/frontend/src/components/pages/QuestionForm.vue
@@ -1,5 +1,65 @@
 <script setup >
     import { defineEmits, ref } from 'vue'
+    import VueCropper from 'vue-cropperjs';
+    import 'cropperjs/dist/cropper.css';
+
+    const cropperRef = ref('');
+    const imageSrc = ref('');
+    const showCropper = ref(false);
+
+    let autoCropWidth = 700;
+    let autoCropHeight = 400;
+
+    const onFileChange = (e) => {
+        imageSrc.value = '';
+        showCropper.value = false;
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+        
+            imageSrc.value = e.target.result;
+            showCropper.value = true;
+            // const img = new Image();
+            // img.src = e.target.result;
+            // showCropper.value = true;
+            // img.onload = () => {
+            //     autoCropWidth = img.width;
+            //     autoCropHeight = img.height;
+            // };
+
+        };
+        reader.readAsDataURL(file);
+
+    };
+
+    const cropImage = async () => {
+        const cropperImage = cropperRef.value;
+        const croppedCanvas = cropperImage.getCroppedCanvas();
+
+        croppedCanvas.toBlob(async (blob) => {
+            const formData = new FormData()
+            formData.append('image', blob)
+
+            fetch(
+                // flask APIのパスを指定
+                "/run_ocr", 
+                {
+                    method: 'POST',
+                    body: formData, //dataをjsonに変換して送信
+                } // postのボディ
+            ).then(res => res.json()).then( 
+                // 同期処理．レスポンス取得後に実行．
+                function(jdata){
+                    var output = jdata["text"];
+                    console.log(`in handleOCR() output: ` + output);
+
+                    question_text.value = output; // scriptTextに出力を代入
+                    console.log(`in handleOCR() question_text.value: ` + question_text.value);
+                    showCropper.value = false;
+                }
+            ).catch(error => console.error('Error:', error)); // エラー処理
+        });
+    };
 
     // textareaの代入先
     const question_text = ref("");
@@ -44,8 +104,19 @@
 <template>
     
 <div >
-    <input type="file" id="imageUpload" />
-    <button class="button-ocr" @click="handleOCR()">Run OCR</button>
+    <input type="file" @change="onFileChange" />
+    <div v-if="showCropper">
+      <vue-cropper
+        ref="cropperRef"
+        :src="imageSrc"
+        :stencil-props="{
+          aspectRatio: 1 / 1,
+        }"
+      ></vue-cropper>
+      <button class="button-ocr" @click="cropImage">画像から読み取る</button>
+    </div>
+    <!-- <input type="file" id="imageUpload" />
+    <button class="button-ocr" @click="handleOCR()">画像から読み取る</button> -->
 </div>
     <textarea class="textarea-01" placeholder="空間図形の問題を入力してください" v-model="question_text" @keydown.ctrl.enter="QuestionSubmit()"></textarea>
     <button class="button-send_to_GPT" @click="QuestionSubmit()">Create GGB Script</button>
@@ -82,6 +153,7 @@
     color: #fff;
     font-weight: 600;
     font-size: 1em;
+    margin-top: 5px;
     margin-bottom: 5px;    /* 下側の余白 */
 }
 .button-ocr:hover {
