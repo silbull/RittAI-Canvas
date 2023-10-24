776cb015b7bfffa325804fbce15eb5b0205464c9
silbull <tmiya5382@gmail.com>
2023-10-24 14:22:44 +0900

    不要ファイル削除

--- function_calling_test/get_wether.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_weather(location, day="tomorrow"):
-    forecast_url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{location}.json"
-    forecast_req = requests.get(forecast_url)
-    return forecast_req
